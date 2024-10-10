c89f26c7979bd44e4ab7dd0adb1c19bf6b8a467f
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-10-10 11:48:34 -0300

    User Change Notifier Provider with new class App Data in main

--- lab01_counter_app/lib/main.dart
+++ lab01_counter_app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lab01_counter_app/models/app_data.dart';
 import 'package:logger/logger.dart';
 import 'package:lab01_counter_app/pages/page_home.dart';
+import 'package:provider/provider.dart';
 
 void main()
 {
@@ -17,16 +19,30 @@ class MyApp extends StatelessWidget
     var logger = Logger();
     logger.d("Logger is working!");
 
-    return MaterialApp
-    (
-      title: 'Counter App',
-      theme: ThemeData
-      (
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
-        useMaterial3: true,
-        fontFamily: 'Oswald'
-      ),
-      home: const MyHomePage(title: 'Aplicación Contador - Página Principal'),
+    return ChangeNotifierProvider<AppData>(
+      create: (context) => AppData(),
+      child: MaterialApp(
+        title: 'Counter App',
+        theme: ThemeData
+        (
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+          useMaterial3: true,
+          fontFamily: 'Oswald'
+        ),
+        home: const MyHomePage(title: 'Aplicación Contador - Página Principal'),
+      )
     );
+
+    // return MaterialApp
+    // (
+    //   title: 'Counter App',
+    //   theme: ThemeData
+    //   (
+    //     colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+    //     useMaterial3: true,
+    //     fontFamily: 'Oswald'
+    //   ),
+    //   home: const MyHomePage(title: 'Aplicación Contador - Página Principal'),
+    // );
   }
 }
