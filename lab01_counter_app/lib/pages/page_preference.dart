0611cac01238c20a29b314029284d014160c71ce
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-10-17 10:05:33 -0300

    Add user name text form field and counter slider

--- lab01_counter_app/lib/pages/page_preference.dart
+++ lab01_counter_app/lib/pages/page_preference.dart
@@ -17,6 +17,8 @@ class PreferencePage extends StatefulWidget
 }
 
 class _PreferencePageState extends State<PreferencePage> {
+  String userName = '';
+  double counter = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +26,46 @@ class _PreferencePageState extends State<PreferencePage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Preferencia'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container( // User name and text form field section.
+              child: Column(
+                children: [
+                  Text('Nombre de usuario: $userName'),
+                  const SizedBox(height: 10,),
+                  TextFormField(
+                    onFieldSubmitted: (value) => {
+                      setState(() {
+                        userName = value;
+                        // print('User name is $userName');
+                      })                      
+                    },
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(height: 25,),
+            Container( // Counter and counter slider section.
+              child: Column(
+                children: [
+                  Text('Contador: ${counter.round().toString()}'),
+                  const SizedBox(height: 10,),
+                  Slider(
+                    value: counter,
+                    max: 100,
+                    onChanged: (value) {
+                      setState(() {
+                        counter = value;
+                      });
+                    }
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
