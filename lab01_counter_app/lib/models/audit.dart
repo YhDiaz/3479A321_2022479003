122f5d8a4b98f47b3bec1a9b707185c9f3232224
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-10-17 11:46:58 -0300

    Add methods to convert to and from JSON file

--- lab01_counter_app/lib/models/audit.dart
+++ lab01_counter_app/lib/models/audit.dart
@@ -7,4 +7,23 @@ class Audit {
     required this.id,
     required this.action
   });
+
+  Map<String, Object?> toJson() => {
+    'id': id,
+    'action': action
+  };
+
+  Audit copy({
+    int? id,
+    String? action
+  }) =>
+      Audit(
+        id: id ?? this.id,
+        action: action ?? this.action
+      );
+
+  factory Audit.fromJson(Map<String, Object?> json) => Audit(
+    id: json['id'] as int,
+    action: json['action'] as String
+  );
 }
