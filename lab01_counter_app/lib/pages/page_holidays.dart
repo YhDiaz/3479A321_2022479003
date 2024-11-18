11c254cce36a6938a9c04aa29b4270a058c49606
Phoenix <71957725+YhDiaz@users.noreply.github.com>
2024-11-18 13:39:09 -0300

    Add title and list of elements to holidays page

--- lab01_counter_app/lib/pages/page_holidays.dart
+++ lab01_counter_app/lib/pages/page_holidays.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 
 class HolidaysPage extends StatefulWidget {
-  const HolidaysPage({super.key});
+  const HolidaysPage({super.key, required this.year});
+
+  final int year;
 
   @override
   State<HolidaysPage> createState() => _HolidaysPageState();
 }
 
 class _HolidaysPageState extends State<HolidaysPage> {
+  List<String> holidays = List.empty(growable: true);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Año ${widget.year}'),
+      ),
+    );
   }
 }
